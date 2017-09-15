4170f91fd0db0a764c8c1828ef607410a877fec0
hardik8085 <hthakkar@uncc.edu>
2017-09-14 22:22:50 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild, ElementRef, AfterViewInit} from '@angular/core';
+import {MenuItem} from "primeng/primeng";
+import {Menu} from "primeng/components/menu/menu";
+import {ActivatedRoute, Router} from "@angular/router";
+declare var jQuery :any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'app';
+  menuItems: MenuItem[];
+  miniMenuItems: MenuItem[];
+  
+
+  @ViewChild('bigMenu') bigMenu : Menu;
+  @ViewChild('smallMenu') smallMenu : Menu;
+
+  ngOnInit() {
+    this.menuItems = [
+      {label: 'Dashboard', icon: 'fa-home', routerLink: ['/dashboard'] },
+      {label: 'All Times', icon: 'fa-calendar', routerLink: ['/alltimes']},
+      {label: 'My Timesheet', icon: 'fa-clock-o', routerLink: ['/timesheet']},
+      {label: 'Add Project', icon: 'fa-tasks', routerLink: ['/projects']},
+      {label: 'My Profile', icon: 'fa-users', routerLink: ['/profile']},
+      {label: 'Settings', icon: 'fa-sliders', routerLink: ['/settings']},
+    ]
+    this.miniMenuItems = [];
+    this.menuItems.forEach( (item : MenuItem) => {
+      let miniItem = { icon: item.icon, routerLink: item.routerLink }
+      this.miniMenuItems.push(miniItem);
+    })
+    }
+    
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MenuModule,PanelModule} from 'primeng/primeng';
 import { AppComponent } from './app.component';
+import { SettingsComponent } from './settings/settings.component';
+import {RouterModule, Routes} from "@angular/router";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+
+const appRoutes: Routes = [
+  { path: "", redirectTo: "", pathMatch: "full" },
+   { path: "settings", component: SettingsComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MenuModule,
+    PanelModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
